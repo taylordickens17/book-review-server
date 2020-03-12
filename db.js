6e2b59062dd8a8ad3cec3adf716e0aeb9de27ce6
TaylorDickens <justindickens17@gmail.com>
2020-03-12 10:35:11 -0400

    deployment

--- db.js
+++ db.js
@@ -1,17 +1,17 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('zebookreviewapp', 'postgres', 'RainbowBrite55!', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres'
 });
 
-sequelize.authenticate().then(
-    function () {
-        console.log('Connected to zebookreviewapp postgres database');
-    },
-    function (err) {
-        console.log(err);
-    }
-);
+sequelize.authenticate().
+    then(
+        function () {
+            console.log('Connected to zebookreviewapp postgres database');
+        },
+        function (err) {
+            console.log(err);
+        }
+    );
 
 module.exports = sequelize;
